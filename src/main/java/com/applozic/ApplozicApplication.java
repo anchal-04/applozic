b14dd720d4469334c1eb6bc82468629ae4cc088c
anchal singh <anchalsingh.narendrapratap@nissanmotor.com>
2020-07-08 16:24:43 +0530

    setting up the project

---
+++ src/main/java/com/applozic/ApplozicApplication.java
@@ -0,0 +1,13 @@
+package com.applozic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplozicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplozicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/applozic/ApplozicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applozic;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplozicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
